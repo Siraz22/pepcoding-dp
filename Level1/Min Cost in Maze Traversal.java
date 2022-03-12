6fa9ce078095eaac768905c7ebb78075daf367c2
siraz <siraznaorem07@gmail.com>
2022-03-12 11:21:39 +0530

    level 1 easy dp done

--- Level1/Min Cost in Maze Traversal.java
+++ Level1/Min Cost in Maze Traversal.java
@@ -3,48 +3,28 @@ import java.util.*;
 
 class Solution {
 
-    public int recursion(int[][] matrix, int row, int col, int curr_row, int curr_col, int[][] dp) {
-        // base condition - reached the end (rightmost wall)
-        if (curr_col == col - 1)
-            return dp[curr_row][curr_col] = matrix[curr_row][curr_col];
-
-        if (dp[curr_row][curr_col] != -1)
-            return dp[curr_row][curr_col];
-
-        int front = recursion(matrix, row, col, curr_row, curr_col + 1, dp);
-
-        int up = -1;
-        int down = -1;
-        if (curr_row != 0) {
-            up = recursion(matrix, row, col, curr_row - 1, curr_col + 1, dp);
-
-        }
-        if (curr_row != row - 1) {
-            down = recursion(matrix, row, col, curr_row + 1, curr_col + 1, dp);
-
-        }
-        return dp[curr_row][curr_col] = (matrix[curr_row][curr_col] + Math.max(front, Math.max(up, down)));
-    }
-
     public Object solve(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
         int[][] dp = new int[row][col];
 
-        for (int i = 0; i < row; ++i) {
-            for (int j = 0; j < col; ++j) {
-                dp[i][j] = -1;
+        dp[0][0] = matrix[0][0];
+
+        for (int i = 1; i < col; ++i) {
+            dp[0][i] = dp[0][i - 1] + matrix[0][i];
+        }
+
+        for (int i = 1; i < row; ++i) {
+            dp[i][0] += dp[i - 1][0] + matrix[i][0];
+        }
+
+        for (int i = 1; i < row; ++i) {
+            for (int j = 1; j < col; ++j) {
+                dp[i][j] = matrix[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
             }
         }
 
-        int answer = -1;
-
-        for (int i = 0; i < row; ++i) {
-            int goldMined = recursion(matrix, row, col, i, 0, dp);
-            answer = Math.max(goldMined, answer);
-        }
-
-        return answer;
+        return dp[row - 1][col - 1];
     }
 }
 
