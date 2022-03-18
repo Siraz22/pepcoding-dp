9eae795b2842b11e840c05b8a6be986d110a031c
siraz <siraznaorem07@gmail.com>
2022-03-18 08:44:37 +0530

    dp

--- Level1/Coin Change Permutation.java
+++ Level1/Coin Change Permutation.java
@@ -1,7 +1,45 @@
-package Level1;
+import java.io.*;
+import java.util.*;
 
-public class Coin
-Change Permutation
-{
+class Solution {
 
+    public Object solve(int[] coins, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i < target + 1; ++i) {
+            dp[i] = 0;
+        }
+
+        for (int i = 1; i < target + 1; ++i) {
+            for (int j = 0; j < coins.length; ++j) {
+                int currCoin = coins[j];
+                int tempIndex = i - currCoin;
+                if (tempIndex >= 0) {
+                    dp[i] += dp[tempIndex];
+                }
+            }
+        }
+
+        return dp[target];
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            int temp = scan.nextInt();
+            arr[i] = temp;
+        }
+
+        int target = scan.nextInt();
+
+        Solution solution = new Solution();
+        Object answer = solution.solve(arr, target);
+        System.out.println(answer.toString());
+    }
 }
--- Main.java
+++ Main.java
@@ -1,38 +1,35 @@
 import java.io.*;
 import java.util.*;
 
-import javax.swing.JLabel;
-
 class Solution {
 
-    public Object solve(int[] coins, int target) {
-        int[][] dp = new int[coins.length + 1][target + 1];
+    public Object solve(int[] value, int[] weight, int target) {
+
+        int[][] dp = new int[weight.length + 1][target + 1];
 
         // initialization
-        for (int i = 0; i < coins.length + 1; ++i) {
-            dp[i][0] = 1;
+        for (int i = 0; i < weight.length + 1; ++i) {
+            dp[i][0] = 0;
         }
-        for (int i = 1; i < target + 1; ++i) {
+        for (int i = 0; i < target + 1; ++i) {
             dp[0][i] = 0;
         }
 
-        for (int i = 1; i < coins.length + 1; ++i) {
+        for (int i = 1; i < weight.length + 1; ++i) {
             for (int j = 1; j < target + 1; ++j) {
 
-                int currTarget = j;
-                int currCoin = coins[i - 1];
+                int currWeight = weight[i - 1];
+                int currCapacity = j;
 
-                if (currCoin <= currTarget) {
-                    // we can choose to take or not take, so two choices
-                    dp[i][j] = dp[i][currTarget - currCoin] + dp[i - 1][currTarget];
+                if (currCapacity >= currWeight) {
+                    // option to choose or not to choose
+                    dp[i][j] = Math.max(value[i - 1] + dp[i][currCapacity - currWeight], dp[i - 1][currCapacity]);
                 } else {
-                    // we can't add this coin
-                    dp[i][j] = dp[i - 1][currTarget];
+                    dp[i][j] = dp[i - 1][currCapacity];
                 }
             }
         }
-
-        return dp[coins.length][target];
+        return dp[weight.length][target];
     }
 }
 
@@ -42,16 +39,23 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-        int[] arr = new int[n];
+        int[] value = new int[n];
+        int[] weight = new int[n];
+
         for (int i = 0; i < n; ++i) {
             int temp = scan.nextInt();
-            arr[i] = temp;
+            value[i] = temp;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            int temp = scan.nextInt();
+            weight[i] = temp;
         }
 
         int target = scan.nextInt();
 
         Solution solution = new Solution();
-        Object answer = solution.solve(arr, target);
+        Object answer = solution.solve(value, weight, target);
         System.out.println(answer.toString());
     }
 }
